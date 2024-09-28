805e75b63ecef00663606a96de48e3b1069007b1
taetkyle <113179398+taetkyle@users.noreply.github.com>
2024-09-28 17:33:48 -0500

    basic page setup

---
+++ lib/MessagePage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Messagepage extends StatefulWidget {
+  const Messagepage({super.key});
+
+  @override
+  State<Messagepage> createState() => _MessagepageState();
+}
+
+class _MessagepageState extends State<Messagepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
